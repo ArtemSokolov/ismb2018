9fb9b51ce34ae0641e762fd1c3a8c5b45240d24e
Artem Sokolov <artem.sokolov@gmail.com>
2018-06-25 11:22:41 -0400

    Began computational setup section

--- render.js
+++ render.js
@@ -4,9 +4,9 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('file:///home/sokolov/projects/ismb2018/output/ismb2018.html');
-    await page.setViewport({width: 1900, height: 1450});
-    await page.screenshot({path: 'test.png'});
-    await page.pdf({path: 'test.pdf', scale:2, width: '40in', height: '31in'});
+//    await page.setViewport({width: 1900, height: 1450});
+//    await page.screenshot({path: 'test.png'});
+    await page.pdf({path: 'test.pdf', scale:2, width: '40in', height: '31in', printBackground: true});
 
     await browser.close();
 })();
