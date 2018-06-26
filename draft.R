d270088a27438e556f785905b168274799741eef
Artem Sokolov <artem.sokolov@gmail.com>
2018-06-25 23:20:25 -0400

    Finalized Size legend

--- draft.R
+++ draft.R
@@ -15,9 +15,10 @@ dgeres <- function()
   LL <- RR %>% filter(Task == "AC") %>% mutate(zoom=TRUE)
   
   ggplot( RR, aes( x = Nienke, y = DGE, col=Task ) ) + theme_bw() +
-    geom_point( size = 2 ) + geom_abline( slope = 1, lty="dashed", lwd=1.2, color="#888888" ) +
+    geom_point( size = 2 ) + geom_abline( slope = 1, lty="dashed", lwd=1.2, color="#999999" ) +
     geom_point( data=LL, aes(size=Size), alpha=0.7 ) +
     xlab( "AUC based on mined sets" ) + ylab( "AUC based on DGE sets" ) +
+    scale_size_continuous( range = c(1,5), breaks = c(10, 25, 50, 100, 200 ), name="Gene Set Size" ) +
     theme( aspect.ratio = 1, strip.text=etxt(12),
            legend.title = etxt(14), legend.text = etxt(12),
            axis.title = etxt(14), axis.text = etxt(12),
@@ -29,8 +30,11 @@ dgeres <- function()
 main <- function()
 {
   gg1 <- dgeres() + guides(size=FALSE) + theme( legend.position = c(0.95, 0.15) )
-  gg2 <- dgeres() + guides(color=FALSE)  + theme( legend.position = c(0.05, 0.85) )
+  gg2 <- dgeres() + guides(color=FALSE)  + theme( legend.position = c(0.075, 0.8) )
   
   lg <- gtable_filter( ggplotGrob(gg2), "guide-box" )$grobs[[1]]
   gg <- ggplotGrob(gg1) %>% gtable_add_grob(lg, 7, 5, 7, 9)
+  
+  grid.newpage()
+  grid.draw(gg)
 }
