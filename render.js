aff94ff768d14466b69fe3afd3b71e0f9567f8d9
Artem Sokolov <artem.sokolov@gmail.com>
2018-06-27 10:32:32 -0400

    Additional figures

--- render.js
+++ render.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
     await page.goto('file:///home/sokolov/projects/ismb2018/output/ismb2018.html');
 //    await page.setViewport({width: 1900, height: 1450});
 //    await page.screenshot({path: 'test.png'});
-    await page.pdf({path: 'test.pdf', scale:2, width: '40in', height: '31in', printBackground: true});
+    await page.pdf({path: 'ismb2018.pdf', scale:2, width: '40in', height: '31in', printBackground: true});
 
     await browser.close();
 })();
